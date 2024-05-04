211d6b528df0d77305bd7778af4e58b21aa96687
toukka404 <toukkakhaled98@gmail.com>
2024-05-04 12:22:25 -0700

    push

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,34 +1,38 @@
 #!/usr/bin/env bash
-# sets up nginx web servers for the deployment of web_static
-apt-get -y update
-apt-get -y install nginx
-ufw allow 'Nginx HTTP'
-mkdir -p /data/web_static/shared/
+# Sets up a web server for deployment of web_static.
+
+apt-get update
+apt-get install -y nginx
+
 mkdir -p /data/web_static/releases/test/
-echo "Hi Max" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R "ubuntu:ubuntu" /data/
-sed -i '1,/error.log;/!d' /etc/nginx/nginx.conf
-echo "
-        server {
-                listen 80 default_server;
-                listen [::]:80 default_server;
-                root /var/www/html;
-                index index.html index.htm index.nginx-debian.html;
-                server_name _;
-                rewrite /redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-                error_page 404 /custom_404.html;
-                location = /custom_404.html {
-                        root /var/www/html;
-                        internal;
-                }
-                location / {
-                        try_files \$uri \$uri/ =404;
-                }
-                location /hbnb_static {
-                        alias /data/web_static/current/;
-                }
-        }
-}" | sudo tee -a /etc/nginx/nginx.conf
-service nginx start
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
 service nginx restart
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
-
-from fabric.api import *
+# Fabfile to generates a .tgz archive from the contents of web_static.
+import os.path
+from datetime import datetime
+from fabric.api import local
 
 
 def do_pack():
-    """compresses web_static/ as a timestamped .tgz
-    """
-    go_to = "versions/web_static_$(date '+%Y%m%d%H%M%S').tgz"
-    local("mkdir -p versions")
-    local(
-        "tar -czvf {} web_static/".format(go_to))
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
